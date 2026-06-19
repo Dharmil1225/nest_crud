10791f5522199f4f023907b6a0e72562b8fdd70c
Dharmil1225 <dharmil.v@simformsolutions.com>
2026-06-19 16:04:26 +0530

    refactor: remove comments and posts modules along with related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { PostModule } from './post/post.module';
-import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, PostModule, CommentModule],
+  imports: [DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
